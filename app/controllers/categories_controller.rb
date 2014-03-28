f8aa37f980c3c1c727c065776d12e60a9ad58e37
Tonko Balkonko <toni.cesarek@gmail.com>
2014-03-28 11:39:57 +0000

    finishes edit and destroy on categories controler and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,43 @@
 class CategoriesController < ApplicationController
-  def index
-	  @categories = Category.all
-  end
+	def index
+		@categories = Category.all
+	end
 
-  def new
-	  @category = Category.new
-  end
+	def new
+		@category = Category.new
+	end
 
-  def create
-	  @category = Category.new(category_params)
+	def create
+		@category = Category.new(category_params)
 
-	  if @category.save
-		  redirect_to categories_path
-		  else
-			  render 'new'
-		  end
-  end
+		if @category.save
+			redirect_to categories_path
+		else
+			render 'new'
+		end
+	end
 
-  def edit
-  end
+	def edit
+		@category = Category.find(params[:id])
+	end
 
-  private
-  def category_params
-	  params.require(:category).permit(:name) 
+	def update
+		@category = Category.find(params[:id])
+		if @category.update(category_params)
+			redirect_to categories_path
+			else
+				render 'edit'
+			end
+	end
+	 def destroy
+		@category = Category.find(params[:id])
+		@category.destroy
+		redirect_to categories_path
+	 end
 
-  end
+	private
+	def category_params
+		params.require(:category).permit(:name) 
+
+	end
 end
