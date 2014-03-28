a37006bdaf86a069ff22967135ee758f78f3d529
Tonko Balkonko <toni.cesarek@gmail.com>
2014-03-28 11:04:24 +0000

    small change for heroku

--- db/migrate/20140325124706_create_subcategories.rb
+++ db/migrate/20140325124706_create_subcategories.rb
@@ -1,7 +1,8 @@
 class CreateSubcategories < ActiveRecord::Migration
   def change
     create_table :subcategories do |t|
-		t.references :category t.string :name
+		t.references :category 
+		t.string :name
 
       t.timestamps
     end
