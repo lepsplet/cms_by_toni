ef83a488e2c468e9e77fd971ab80067432a9ea01
tonko <toni.cesarek@gmail.com>
2014-03-29 20:19:31 +0100

    working on subcategories to work

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,8 +1,11 @@
 class SubcategoriesController < ApplicationController
   def index
+	  @category = Category.find(params[:id])
   end
 
   def new
+	  @category = Category.find(params[:id])
+	  @subcategory = @category.subcategories.build
   end
 
   def edit
