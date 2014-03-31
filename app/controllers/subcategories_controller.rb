a6790042e33e0028795018bc40b97250732bdb05
Tonko Balkonko <toni.cesarek@gmail.com>
2014-03-31 14:34:33 +0000

    nekaj ne štima z linki

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,13 +1,30 @@
 class SubcategoriesController < ApplicationController
-  def index
-	  @category = Category.find(params[:id])
-  end
+	def index
+		@category = Category.find(params[:id])
+	end
 
-  def new
-	  @category = Category.find(params[:id])
-	  @subcategory = @category.subcategories.build
-  end
+	def new
+		@category = Category.find(params[:id])
+		@subcategory = @category.subcategories.build
+	end
 
-  def edit
-  end
+	def create
+		@category = Category.find(params[:id])
+		@subcategory = @category.subcategories.build(subcategory_params)
+
+		if @subcategory.save
+			redirect_to subcategories_path(id: @subcategory.id)
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	private
+	def subcategory_params
+		params.require(:subcategory).permit(:name) 
+
+	end
 end
