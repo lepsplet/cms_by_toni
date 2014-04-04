abfaf829dca42ce5e772a15de1c5378ee4a04e52
Tonko Balkonko <toni.cesarek@gmail.com>
2014-04-04 09:02:39 +0000

    adds validations to all inputs on the cms side

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many :subcategories
+
+	validates :name, length: { minimum: 5, maximum: 20 }
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,4 +1,6 @@
 class Subcategory < ActiveRecord::Base
 	belongs_to :category
 	has_many :articles
+
+	validates :name, length: { minimum: 5, maximum: 20 }
 end
